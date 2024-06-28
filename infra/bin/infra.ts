e6d8b4c77e21f84b56c748c97f947c20d5b57920
Zain Lakhani <lakhani.zain786@gmail.com>
2024-06-28 11:03:16 -0400

    Update

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -2,17 +2,9 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { InfraStack } from '../lib/infra-stack';
-import { DevInfraStack } from '../lib/dev-infra-stack';
 
 const app = new cdk.App();
-new InfraStack(app, 'AlphaEDU-Rewards', {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
-});
-
-new DevInfraStack(app, 'AlphaEDU-Rewards-Dev', {
+new InfraStack(app, 'Alpha1Edtech', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -6,6 +6,7 @@ import * as certs from 'aws-cdk-lib/aws-certificatemanager';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
 import * as ecrAsset from 'aws-cdk-lib/aws-ecr-assets';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import * as rds from 'aws-cdk-lib/aws-rds';
 import * as logs from 'aws-cdk-lib/aws-logs';
 import * as route53 from 'aws-cdk-lib/aws-route53';
@@ -13,6 +14,7 @@ import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 import * as path from 'path';
 import { execSync } from 'child_process';
 import { version } from '../../package.json';
+import 'dotenv';
 
 const env = process.env.NODE_ENV || 'production';
 
@@ -46,7 +48,7 @@ export class InfraStack extends cdk.Stack {
     super(scope, id, props);
 
     // jump back to the `rewards` directory
-    const rewardsDockerfilePath = path.join(__dirname, '..', '..');
+    const InceptDockerFilePath = path.join(__dirname, '..', '..');
     // get the git sha
     const revision = getGitRevision();
     const dirty = getDirtyState();
@@ -56,13 +58,13 @@ export class InfraStack extends cdk.Stack {
       vpcId: 'vpc-0587bce4face422f4',
     });
 
-    const registry = new ecr.Repository(this, 'rewards-registry', {
+    const registry = new ecr.Repository(this, 'incept-registry', {
       imageTagMutability: ecr.TagMutability.IMMUTABLE,
-      repositoryName: 'rewards-repo',
+      repositoryName: '1edtech-repo',
     });
 
-    const rdsMasterUser = new Secret(this, 'rewards-db-user-creds', {
-      secretName: 'rewards-db-user-credentials',
+    const rdsMasterUser = new Secret(this, 'incept-core-db-user-creds', {
+      secretName: '1edtech-core-db-user-credentials',
       description: 'Master Creds for Croe DB',
       generateSecretString: {
         secretStringTemplate: JSON.stringify({ username: 'postgres' }),
@@ -72,8 +74,8 @@ export class InfraStack extends cdk.Stack {
       },
     });
 
-    const dbSg = new ec2.SecurityGroup(this, 'rewards-db-sg', {
-      securityGroupName: 'rewards-db',
+    const dbSg = new ec2.SecurityGroup(this, 'incept-core-db-sg', {
+      securityGroupName: '1edtech-core-db',
       vpc,
     });
 
@@ -83,11 +85,11 @@ export class InfraStack extends cdk.Stack {
       `Allow port 5432 from within vpc`,
     );
 
-    const rewardsDb = new rds.DatabaseCluster(this, 'rewards-db', {
-      clusterIdentifier: 'rewardsdb',
-      defaultDatabaseName: 'rewardsdb',
-      engine: rds.DatabaseClusterEngine.auroraPostgres({
-        version: rds.AuroraPostgresEngineVersion.VER_15_3,
+    const coreDb = new rds.DatabaseCluster(this, 'incept-core-db', {
+      clusterIdentifier: 'edtechcore',
+      defaultDatabaseName: 'core',
+      engine: rds.DatabaseClusterEngine.auroraMysql({
+        version: rds.AuroraMysqlEngineVersion.VER_3_04_0,
       }),
       backup: {
         retention: cdk.Duration.days(14),
@@ -96,7 +98,6 @@ export class InfraStack extends cdk.Stack {
       instances: 1,
       iamAuthentication: true,
       copyTagsToSnapshot: true,
-      cloudwatchLogsExports: ['postgresql'],
       cloudwatchLogsRetention: logs.RetentionDays.ONE_MONTH,
       instanceProps: {
         vpc,
@@ -112,43 +113,37 @@ export class InfraStack extends cdk.Stack {
       credentials: rds.Credentials.fromSecret(rdsMasterUser),
     });
 
-    new cdk.CfnOutput(this, 'Rewards-DB-Endpoints', {
-      exportName: `${rewardsDb.stack.stackName}:Endpoints`,
-      value: rewardsDb.instanceEndpoints.join(','),
+    new cdk.CfnOutput(this, 'incept-core-db-Endpoints', {
+      exportName: `${coreDb.stack.stackName}:Endpoints`,
+      value: coreDb.instanceEndpoints.join(','),
     });
 
-    new cdk.CfnOutput(this, 'Rewards-DB-Cluster-Endpoint', {
-      exportName: `${rewardsDb.stack.stackName}:Endpoint`,
-      value: rewardsDb.clusterEndpoint.socketAddress,
+    new cdk.CfnOutput(this, 'incept-core-db-Cluster-Endpoint', {
+      exportName: `${coreDb.stack.stackName}:Endpoint`,
+      value: coreDb.clusterEndpoint.socketAddress,
     });
 
     const hostedZone = route53.HostedZone.fromHostedZoneAttributes(
       this,
-      'alphaflashcards-hostedzone',
+      'incept-hostedzone',
       {
-        zoneName: 'alphaflashcards.com',
-        hostedZoneId: 'Z01068543RZNP42QJ3RME',
+        zoneName: 'alpha1edtech.com',
+        hostedZoneId: 'Z07100071SI52GQ72WJ93',
       },
     );
 
-    // cert for api.alphaflashcards.com
-    // cert for *.alphaflashcards.com
-    // we generated this via the UI because we don't want to run into a situation where a change here
-    // results in a whole new SSL cert being generated for any reason.
-    // We also have *.alphaflashcards.com as a SAN so we want to ensure that
-    // it doesn't get changed for any other stacks.
-    // Also validating the cert can take some unknown amount of time and we don't want
-    // a deploy to hang because of an unexpected cert update.
-    // Why didn't we put it in the infra stack?: Same reasons, but we can port it there eventually.
     const wildcardSSLCertARN =
-      'arn:aws:acm:us-east-1:597610889212:certificate/65b0e636-75ab-481f-ac25-380caa1db3ea';
+      'arn:aws:acm:us-east-1:597610889212:certificate/6bf5292b-e47e-4885-bd9a-6c84c40bb8ee';
     const wildcardSSLCert = certs.Certificate.fromCertificateArn(
       this,
       'wildcard-ssl-cert',
       wildcardSSLCertARN,
     );
-    const rewardsRestService =
-      new pattern.ApplicationLoadBalancedFargateService(this, 'rewards', {
+
+    const inceptRestService = new pattern.ApplicationLoadBalancedFargateService(
+      this,
+      'incept-core-service',
+      {
         vpc,
         memoryLimitMiB: 1024,
         // @TODO: change this count to be dynamic for prod deploys
@@ -156,7 +151,7 @@ export class InfraStack extends cdk.Stack {
         cpu: 512,
         securityGroups: [dbSg],
         taskImageOptions: {
-          image: ecs.ContainerImage.fromAsset(rewardsDockerfilePath, {
+          image: ecs.ContainerImage.fromAsset(InceptDockerFilePath, {
             //@TODO: for some reason this is NOT building the production target, but they are the same functionally
             target: 'dev',
             platform: ecrAsset.Platform.LINUX_AMD64,
@@ -169,78 +164,28 @@ export class InfraStack extends cdk.Stack {
             CORE_VERSION: version,
             GIT_SHA: `${revision}:${dirty.length ? 'dirty' : 'clean'}`,
             GIT_BRANCH: branchName,
-            REWARDS_API_KEY: process.env.REWARDS_API_KEY || '',
-            REWARDS_API_URL: process.env.REWARDS_API_URL || '',
           },
-          containerName: 'rewards',
-          containerPort: 3000,
+          containerName: '1edtech',
+          containerPort: 3010,
         },
-        loadBalancerName: 'alphaedu-rewards-lb',
-        domainName: 'rewards.api.alphaflashcards.com',
-        domainZone: hostedZone,
-        certificate: wildcardSSLCert,
-        publicLoadBalancer: true,
-      });
-
-    // OK I don't like this.. the problem is that the way the hasura container
-    // needs to be configured is via environment variable
-    const host = rdsMasterUser.secretValueFromJson('host').unsafeUnwrap();
-    const user = rdsMasterUser.secretValueFromJson('username').unsafeUnwrap();
-    const pass = rdsMasterUser.secretValueFromJson('password').unsafeUnwrap();
-    const dbname = rdsMasterUser.secretValueFromJson('dbname').unsafeUnwrap();
-
-    const hasuraService = new pattern.ApplicationLoadBalancedFargateService(
-      this,
-      'hasura',
-      {
-        vpc,
-        memoryLimitMiB: 1024,
-        // @TODO: change this count to be dynamic for prod deploys
-        desiredCount: 1,
-        cpu: 512,
-        securityGroups: [dbSg],
-        //@TODO: temporary to force the system to wait because of the funky restart process
-        healthCheckGracePeriod: cdk.Duration.minutes(3),
-        taskImageOptions: {
-          image: ecs.ContainerImage.fromAsset(
-            path.join(__dirname, '..', '..'),
-            {
-              file: 'hasura-deploy.Dockerfile',
-              platform: ecrAsset.Platform.LINUX_AMD64,
-            },
-          ),
-          secrets: {
-            DB: ecs.Secret.fromSecretsManager(rdsMasterUser),
-          },
-          environment: {
-            NODE_ENV: env,
-            CORE_VERSION: version,
-            GIT_SHA: `${revision}:${dirty.length ? 'dirty' : 'clean'}`,
-            GIT_BRANCH: branchName,
-            // hasura specific env vars
-            HASURA_GRAPHQL_DATABASE_URL: `postgres://${user}:${pass}@${host}:5432/${dbname}`,
-            PG_DATABASE_URL: `postgres://${user}:${pass}@${host}:5432/${dbname}`,
-            HASURA_GRAPHQL_ENABLE_CONSOLE: 'true',
-            HASURA_GRAPHQL_DEV_MODE: 'true',
-            HASURA_GRAPHQL_ENABLE_TELEMETRY: 'false',
-            HASURA_GRAPHQL_ENABLED_LOG_TYPES:
-              'startup, http-log, webhook-log, websocket-log, query-log',
-            PORT: '8080',
-          },
-          containerName: 'hasura-graphql-engine',
-          containerPort: 8080,
-        },
-        loadBalancerName: 'rewards-hasura-alb',
-        domainName: 'rewards.graphql.alphaflashcards.com',
+        loadBalancerName: '1edtech-lb',
+        domainName: 'core.alpha1edtech.com',
         domainZone: hostedZone,
         certificate: wildcardSSLCert,
         publicLoadBalancer: true,
       },
     );
 
-    hasuraService.targetGroup.healthCheck = {
-      path: '/healthz',
-      port: '8080',
-    };
+    inceptRestService.taskDefinition.taskRole.attachInlinePolicy(
+      new iam.Policy(this, 's3-access-policy', {
+        statements: [
+          new iam.PolicyStatement({
+            actions: ['s3:*'],
+            effect: iam.Effect.ALLOW,
+            resources: ['*'],
+          }),
+        ],
+      }),
+    );
   }
 }
